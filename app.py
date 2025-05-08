901dd41812be4bc4ffcc3479431e95b7544c2ca2
cgrigo <grigori7519@gmail.com>
2025-05-08 16:21:05 +0300

    formula 1 project

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
@@ -20,6 +20,10 @@ def about():
 @app.route("/services")
 def services():
     return render_template("services.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
